6d84af26908043742f47101ae96a692f21eab85f
Álex Román Núñez <eirexe123@gmail.com>
2021-09-22 15:59:07 +0200

    Fix A-Pose shenanigans in VRMAnimator

--- runtime/VRMAnimator.gd
+++ runtime/VRMAnimator.gd
@@ -10,6 +10,17 @@ func _ready():
 	vrm = get_child(0)
 	
 	skeleton = vrm.get_node(vrm.vrm_skeleton) as Skeleton
+	var right_arm_bone = find_humanoid_bone("rightUpperArm")
+	var left_arm_bone = find_humanoid_bone("leftUpperArm")
+	if right_arm_bone != -1:
+		var br = skeleton.get_bone_rest(right_arm_bone)
+		br.basis = Basis(Vector3.FORWARD, deg2rad(30)) * br.basis
+		skeleton.set_bone_rest(right_arm_bone, br)
+	
+	if left_arm_bone != -1:
+		var br = skeleton.get_bone_rest(left_arm_bone)
+		br.basis = Basis(Vector3.FORWARD, deg2rad(-30)) * br.basis
+		skeleton.set_bone_rest(left_arm_bone,  br)
 	
 func find_humanoid_bone(bone_name: String):
 	if bone_name in vrm.vrm_meta.humanoid_bone_mapping:
